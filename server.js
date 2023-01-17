1094abb0d7077f5644a3aa77f2de1a50e592ec9c
LindsayLew <lmschwehr@gmail.com>
2023-01-16 18:38:27 -0800

    server is mostly set up, need to do my npms

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const session = require('express-session');
 const routes = require('./routes');
 
 const sequelize = require('./config/connection');
+const { appendFile } = require('fs');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -20,3 +21,14 @@ const sess = {
         db: sequelize
     })
 };
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
