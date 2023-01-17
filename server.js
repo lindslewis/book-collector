e253b9586317ed13d6f009d2e79e69a03c721e73
LindsayLew <lmschwehr@gmail.com>
2023-01-16 18:35:33 -0800

    server.js continued, session

--- server.js
+++ server.js
@@ -10,3 +10,13 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Protected access pages',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
