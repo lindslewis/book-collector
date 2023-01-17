bc0b762a44043a8f0ecf8bfe23d2fbf85c06a7aa
LindsayLew <lmschwehr@gmail.com>
2023-01-16 18:34:14 -0800

    setting up boilerplate server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+// use boilerplate server for now. 
+// port is 3001
+
+const express = require('express');
+const session = require('express-session');
+const routes = require('./routes');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
